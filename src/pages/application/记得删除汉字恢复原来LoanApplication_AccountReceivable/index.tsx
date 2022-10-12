c7570694b4585dfd151126e0ccbad2cabea09be8
Tomlili43 <2539081408@qq.com>
2022-10-12 15:36:48 +0800

    任务初始化

--- src/pages/application/记得删除汉字恢复原来LoanApplication_AccountReceivable/index.tsx
+++ src/pages/application/记得删除汉字恢复原来LoanApplication_AccountReceivable/index.tsx
@@ -6,18 +6,52 @@ import ProForm, {
   ProFormMoney,
   ProFormDatePicker,
   ProFormDigit,
+  ProFormInstance,
   ProFormDependency,
 } from '@ant-design/pro-form';
-import { useRequest, FormattedMessage } from 'umi';
+import { useRequest, FormattedMessage, request, useAccess } from 'umi';
 import type { FC } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { fakeSubmitForm } from './service';
+import { fakeSubmitForm, loanApplication } from './service';
 import styles from './style.less';
+import {parse} from 'querystring'
 
-import { history, request } from 'umi';
+
+
+import { history } from 'umi';
+import { useRef } from "react";
 import moment from 'moment';
+const dateFormat = 'YYYY-MM-DD';
+var loan_term = 6
+
+
+
 
 const CompanyForm: FC<Record<string, any>> = () => {
+
+  const formRef = useRef<ProFormInstance>();
+  const platformIdChange = (value: any) =>{
+    console.log(value);
+    if(value =="7"){
+      loan_term = 6
+    }else if(value =="14"){
+      loan_term = 13
+    }else if(value =="30"){
+      loan_term = 29
+    }else if(value =="45"){
+      loan_term = 44
+    }else if(value =="60"){
+        loan_term = 59
+    }else{loan_term = 89}
+
+    formRef?.current?.setFieldsValue({
+      // name: "张三",
+      application_end_date: moment().add(loan_term,'d').format(dateFormat)
+    });
+    // console.log("formRef++++++++++",formRef?.current?.getFieldFormatValue("application_loan_term"));
+  }
+
+
   const { run } = useRequest(fakeSubmitForm, {
     manual: true,
     onSuccess: () => {
@@ -27,14 +61,18 @@ const CompanyForm: FC<Record<string, any>> = () => {
 
   const onFinish = async (values: Record<string, any>) => {
     run(values);
+    // 跳转到指定路由
+    //window.open("/application/loan-application-list-borrower", "_self");
     history.push('/application/loan-application-list-borrower');
   };
 
-  const { data, error, loading } = useRequest(() => {
+  var { data, error, loading } = useRequest(() => {
     return request(
       '/api/borrower/get_borrower_from_id',
     );
   });
+  
+  const access = useAccess();
 
   var checkMoney = (rule, value, callback) => {
     if (!value) {
@@ -55,10 +93,12 @@ const CompanyForm: FC<Record<string, any>> = () => {
     }, 100);
   };
 
+
   return (
     <PageContainer content="">
       <Card bordered={false}>
         <ProForm
+          formRef={formRef}
           layout="vertical"
           onFinish={onFinish}
         >
@@ -82,18 +122,18 @@ const CompanyForm: FC<Record<string, any>> = () => {
                 </Descriptions>
             </div>
           </Card>
-
-          <Card 
-            title={<FormattedMessage id='pages.util.basic_info'/>} 
-            className={styles.card} 
+          {/* <Card title={<FormattedMessage id='pages.util.basic_info'/>} className={styles.card} bordered={false}> */}
+          <Card
+            title={<FormattedMessage id='pages.util.basic_info'/>}
+            className="styles.card"
             bordered={false}
             size= "small"
-            headStyle = {{color:'#2f54eb', fontSize: 16, fontWeight:'bold'}}            
+            headStyle = {{color:'#2f54eb', fontSize: 16, fontWeight:'bold'}}
             >
             <Row gutter={16}>
               <Col xl={6} lg={6} md={12} sm={24}>
                 <ProFormText
-                  label={<FormattedMessage id='pages.util.name'/>}
+                  label={<FormattedMessage id='pages.loan_application_list.purpose'/>}
                   width="md"
                   name="name"
                   rules={[{ required: true, message: 'Please input the application name.' }]}
@@ -108,107 +148,108 @@ const CompanyForm: FC<Record<string, any>> = () => {
               
             </Row>
 
-          <Row gutter={16}>
-            <Col xl={6} lg={6} md={12} sm={24}>
-              <ProFormMoney
-                label={<FormattedMessage id='pages.loan_form.total_amount'/>}
-                width="md"
-                name="application_amount"
-                locale="en-US"
-                rules={[{ required: true, message: 'Please input the total amount.' },
-                        { validator: checkMoney }]}
-              />
-            </Col>
-
-            <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 12 }} sm={24}>
-              <ProFormSelect
-                label={<FormattedMessage id='pages.util.currency'/>}
-                width="md"
-                name="application_currency"
-                rules={[{ required: true, message: 'Please input the currency.' }]}
-                valueEnum={{
-                  USD: 'USD',
-                  HKD: 'HKD',
-                }}
-              />
-            </Col>
-
-            <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 24 }} sm={24}>
-            </Col>
-          </Row>
-
-
-          <Row gutter={16}>
-            <Col xl={6} lg={6} md={12} sm={24}>
-                <ProFormDatePicker
-                  label={<FormattedMessage id='pages.util.start_date'/>}
+            <Row gutter={16}>
+              <Col xl={6} lg={6} md={12} sm={24}>
+                <ProFormMoney
+                  label={<FormattedMessage id='pages.loan_form.total_amount'/>}
                   width="md"
-                  name="application_start_date"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Please select the start date',
-                    },
-                  ]}
-                  //initialValue={moment().format('YYYY-MM-DD')}
-                />                
-            </Col>
+                  name="application_amount"
+                  fieldProps={{ moneySymbol: false, }}
+                  locale="en-US"
+                  rules={
+                    [{ required: true, message: 'Please input the total amount.' },
+                     { validator: checkMoney,  }
+                    ]
+                  }
+                />
+              </Col>
 
-            <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 12 }} sm={24}>
+              <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 12 }} sm={24}>
+                <ProFormSelect
+                  initialValue={data?.currency}
+                  label={<FormattedMessage id='pages.util.currency'/>}
+                  width="md"
+                  name="application_currency"
+                  rules={[{ required: true, message: 'Please input the currency.' }]}
+                  valueEnum={{
+                    HKD: 'HKD',
+                    CNY: 'CNY',
+                    USD: 'USD',
+                  }}
+                />
+              </Col>
+
+              <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 24 }} sm={24}>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col xl={6} lg={6} md={12} sm={24}>
+              
               <ProFormSelect
+                //国际化信息
+                fieldProps={{
+                  onChange: (e) => {
+                    console.log("platformIdChange(e)")
+                    platformIdChange(e);
+                  },
+                }}
                 label={<FormattedMessage id='pages.util.loan_term'/>}
                 width="md"
                 name="application_loan_term"
+                // name="application_currency"
                 rules={[{ required: true, message: 'Please input the loan term.' }]}
                 valueEnum={{
                   7:7,
                   14:14,
                   30:30,
                   45:45,
-                  60:60,
-                  90: 90,
-                  120: 120,
                 }}
-                initialValue={30}
               />
-            </Col>
+              </Col>
 
-            <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 24 }} sm={24}>
-              <ProFormDependency name={['application_start_date', 'application_loan_term']}>
-              {({application_start_date, application_loan_term}) => {
-                let date = new Date(application_start_date);
-                date.setDate(date.getDate() + Number(application_loan_term));
-                if (application_start_date != null) {
-                  return (
-                    <ProFormText
-                      label={<FormattedMessage id='pages.util.end_date'/>}
-                      width="md"
-                      name="application_end_date"
-                      rules={[{ required: true, message: '' }]}
-                      value={moment(date).format('YYYY-MM-DD')}
-                      disabled
-                    />
-
-/*                    
+              <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 24 }} sm={24}>
                   <ProFormDatePicker
-                    label={<FormattedMessage id='pages.util.end_date'/>}
+                    initialValue={moment().format(dateFormat)} 
+                    disabled
+                    // moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+                    label={<FormattedMessage id='pages.util.start_date'/>}
                     width="md"
-                    name="application_end_date"
+                    name="application_start_date"
                     rules={[
                       {
                         required: true,
-                        message: 'Please select the end date',
+                        message: 'Please select the start date',
                       },
                     ]}
-                    value={date}
-                  />
-*/                  
-                  );
-                }
-              }}
-              </ProFormDependency>                
-            </Col>
-          </Row>
+                  />                
+              </Col>
+
+              <Col xl={{ span: 6, offset: 2 }} lg={{ span: 6 }} md={{ span: 12 }} sm={24}>
+                <ProFormDependency name={['application_loan_term']}>
+                    {({application_loan_term}) => {
+                        return (          
+                          <ProFormDatePicker
+                          // initialValue = {moment().format(dateFormat)}
+                              disabled
+                              initialValue={moment().add(loan_term,'d').format(dateFormat)}
+                              label={<FormattedMessage id='pages.util.end_date'/>}
+                              width="md"
+                              name="application_end_date"
+                              rules={[
+                                {
+                                  required: true,
+                                  message: 'end_date',
+                                },
+                              ]}
+                        />  
+                        );
+                    }}
+                </ProFormDependency> 
+                          
+              </Col>
+          
+            </Row>
         </Card>
 
         <Card title={<FormattedMessage id='pages.util.other'/>} className={styles.card} bordered={false}>
